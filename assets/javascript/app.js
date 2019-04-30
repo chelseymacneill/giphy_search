6344b57b9b2d9475e7bc63553950ab254b7e9d2c
cmac <chelsey.macneill@gmail.com>
2019-04-30 14:32:07 -0700

    When a team button is clicked an API call is made with that team as the search query. Currently just displays JSON to the console.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,69 @@
 
+// Feature list
+// If I have time I'll limit it to teams in the 2019 NBA playoffs
+
+
 // A $( document ).ready() block.
 $( document ).ready(function() {
     
     // create an array of strings. Save it to a variable called `topics`.
-    var players = ['Charles Barkley', 'Draymond Green', 'Rasheed Wallace', 'Ben Wallace']
+    var teams = ['Golden State Warriors', 'Houston Rockets', 
+    'Miluakee Bucks', 'Philedelphia Seventy Sixers', 'Portland TrailBlazers']
+    
+    //var players = ['Charles Barkley', 'Draymond Green', 'Rasheed Wallace', 'Ben Wallace']
     
     // take the topics in this array and create buttons in your HTML.
-    function createPlayersButtons () {
-        for (var i = 0; i < players.length; i++) {
+    function createTeamsButtons () {
+        for (var i = 0; i < teams.length; i++) {
             // Creates a new button
             var b = $('<button>');
             // Add a class to the new button
-            b.addClass('player');
+            b.addClass('team');
             // Add a data attribute for each index of topics
-            b.attr('data-playerName', players[i]);
+            b.attr('data-teamName', teams[i]);
             // Text that you want to display on the button
-            b.text(players[i]);
+            b.text(teams[i]);
             // Append the button to the html document
-            $('#players').append(b);
+            $('#teams').append(b);
             console.log(b)
         };
     };
     
     //When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
-    // Ajax call to grab the gifs
-    var apiKey = '6uwYL0toyPhGto5jWNPJvt5LRwOuCpCL';
-    var apiLimit = 10;
-    var apiCallUrl = 'http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=' + apiKey + '&limit=' + apiLimit;
+    function displayTeamGifs () {
+        
+    }
     
-    $.ajax({
-        // buiild the url for the api call 
-        url : apiCallUrl,
-        method : "GET"
-    }).then (function(apiResponse) {
-       //Function to manipulate response goes here
-       console.log(apiResponse);
-    });
     
-    // Pause function 
-    $('#display').on("click", function stopGIF() {});
+    
+    // Wrap the ajax call in a function that will be called by other functions that are triggered by button clicks
+    
+    function passTeamsToAPICall() {
+        var apiKey = '6uwYL0toyPhGto5jWNPJvt5LRwOuCpCL';
+        var apiLimit = 10;
+
+        for ( var i = 0; i < teams.length; i++) {
+        // Ajax call to grab the gifs
+        var q = teams[i];
+        var apiCallUrl = 'http://api.giphy.com/v1/gifs/search?q='+ q + '&api_key=' + apiKey + '&limit=' + apiLimit;
+        
+        $.ajax({
+            // buiild the url for the api call 
+            url : apiCallUrl,
+            method : "GET"
+        }).then (function(apiResponse) {
+            //Function to manipulate response goes here
+            console.log(apiResponse);
+            console.log(q)
+            console.log(apiCallUrl)
+            
+        });
+
+    }
+    };
+        
+        // Pause function 
+        $('#display').on("click", function stopGIF() {});
         
         //When the user clicks one of the still GIPHY images, the gif should animate. 
         $('.gif').on('click', function() { // gif is not an html element yet
@@ -53,28 +78,27 @@ $( document ).ready(function() {
             } else $(this).attr('src', $(this).attr('data-still'));
             $(this).attr('data-state', 'still')
         });
-        
-        
-        // Under every gif, display its rating (PG, G, so on).
-        
-        //* Only once you get images displaying with button presses should you move on to the next step.
-        
-        // Add a form to your page takes the value from a user input box and adds it into your `topics` array. Then make a function call that takes each topic in the array remakes the buttons on the page.
-        $('add-player-button').on('click', function(event) {
-            // Stops the default behavior of the input from overriding our desired behavior
-            event.preventDefault();
-            // Grab the text from the user input
-            var player = $('player-input').val().trim(); // why is this val and not value
-            // Push this name to the array of players 
-            players.push(player);
-            
-        });
-        // Deploy your assignment to Github Pages.
-        
-        
-        
-        
-        // Call CreatePlayerButtons function to build the default buttons
-        createPlayersButtons();
-    }); // end of the document ready block
+
     
+    // Under every gif, display its rating (PG, G, so on).
+    
+    //* Only once you get images displaying with button presses should you move on to the next step.
+    
+    // Add a form to your page takes the value from a user input box and adds it into your `topics` array. Then make a function call that takes each topic in the array remakes the buttons on the page.
+    $('add-team-button').on('click', function(event) {
+        // Stops the default behavior of the input from overriding our desired behavior
+        event.preventDefault();
+        // Grab the text from the user input
+        var team = $('team-input').val().trim(); // why is this val and not value
+        // Push this name to the array of teams 
+        teams.push(team);
+        
+    });
+    // Deploy your assignment to Github Pages.
+    
+    
+    // On click of a button with class team display gifs for that team
+    $(document).on("click", ".team", passTeamsToAPICall);
+    // Call CreateteamButtons function to build the default buttons
+    createTeamsButtons();
+}); // end of the document ready block
